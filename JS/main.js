3b49e61aae04f610c95bd8b74f90e63124b4468f
Alexey <alexeykarpenko2015@gmail.com>
2023-04-19 08:33:36 +0100

    some changes

--- JS/main.js
+++ JS/main.js
@@ -3,61 +3,27 @@ let numberM;
 let sum = 0;
 
 do {
-      numberN = parseInt(prompt("Введите число N", 0), 10);
-      numberM = parseInt(prompt("Введите число M", 0), 10);
-} while ((Number.isNaN (numberN)) || (Number.isNaN(numberM)));
+  numberN = parseInt(prompt("Введите число N", 0),10);
+} while ((Number.isNaN (numberN)) || numberN <= 0);
 
-let result = confirm("Пропускать парные числа?");
+do {
+  numberM = parseInt(prompt("Введите число M", 0), 10);
+} while ((Number.isNaN (numberN)) || numberM <= 0);
+
+let skipEvenNumbers = confirm("Пропускать парные числа?");
+
 for (let i = numberN; i <= numberM; i++) {
-      
-  if(!result) {
+  if(!skipEvenNumbers) {
+    sum=sum+i;
+  } else if (i % 2 == 1) {
       sum=sum+i;
-
-} else if (i%2==1) {
-      sum=sum+i;
+    }
 }
-}
-console.log(sum); 
+console.log(sum);
+   
 
-// Этот код работает, но мне кажется что лучше его записать как написано ниже, только непонятно как записать правильно.
-
-// let numberN;
-// let numberM;
-// let sum = 0;
-
-// do {
-//       numberN = parseInt(prompt("Введите число N", 0), 10);
-//       numberM = parseInt(prompt("Введите число M", 0), 10);
-// } while ((Number.isNaN (numberN)) || (Number.isNaN(numberM)));
-
-
-// let result = confirm("Пропускать парные числа?");
-// for (let i = numberN; i <= numberM; i++) {
-      
-//   if(!result) {
-//       sum=sum+i;
-
-// } else {
-//       sum=sum + (i%2==1);
-// }
-// }
-// console.log(sum);     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// N = 10, M = 100, skipEvenNumbers = true —> 2475
+// N = 10, M = 100, skipEvenNumbers = false —> 5005
+// N = 10.456, M = 100.987, skipEvenNumbers = false —> 5005
+// N = any letters/0 -> запрашивает N снова
+// N = 10, M = any letters/0 ->  запрашивает M снова
